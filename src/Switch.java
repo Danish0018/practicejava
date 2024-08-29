d5afea6f23e870ed780c3b918292a2d719cb9e56
Your Name <you@example.com>
2024-08-29 17:32:25 +0530

    Switch

---
+++ src/Switch.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        switch(button){
+            case 1 : System.out.println("Adil");
+            break;
+            case 2 : System.out.println("Danish");
+            break;
+            case 3 : System.out.println("Wase");
+            break;
+            case 4 : System.out.println("Saqib");
+            break;
+            default: System.out.println("Invalid button");
+        }
+
+    }
+}
